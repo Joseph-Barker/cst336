e7d976f9399bdedd540c3c4e6e8cc3caebc5cd8b
Joseph Barker <jbarker@csumb.edu>
2019-03-03 01:01:19 +0000

    lab 4

---
+++ labs/lab5/basic.php
@@ -0,0 +1,16 @@
+<?php
+
+$otterId = "good9016"
+echo "$otterId;
+
+$newArray = array("one", "two", "three");
+
+$weekdays = array();
+
+$weekdays[] = "Monday";
+
+array_push($weekdays, "Wednesday", "Thursday", "Friday");
+
+
+
+?>
