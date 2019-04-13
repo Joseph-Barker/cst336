a75628e2f5b2f68bfc35fcc33f5bba6f6dc2b3db
Joseph Barker <jbarker@csumb.edu>
2019-04-13 05:48:31 +0000

    lab 7.1

--- labs/lab6/api/getProducts.php
+++ labs/lab6/api/getProducts.php
@@ -7,6 +7,15 @@ $dbname = "ottermart";
 $username = "root";
 $password = "";
 
+//checks whether the URL contains "herokuapp" (using Heroku)
+if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$host = $url["host"];
+$dbName = substr($url["path"], 1);
+$username = $url["user"];
+$password = $url["pass"];
+}
+
 // Establishing a connection
 $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
 
